b044670bd6e1097b19ee080e5b0d9db317b73a99
gaotiefeng <1096392101@qq.com>
2020-03-28 15:04:54 +0800

    add middleware

--- app/Kernel/Http/Response.php
+++ app/Kernel/Http/Response.php
@@ -13,7 +13,10 @@ declare(strict_types=1);
 namespace App\Kernel\Http;
 
 use Hyperf\Contract\ContainerInterface;
+use Hyperf\HttpMessage\Cookie\Cookie;
 use Hyperf\HttpServer\Contract\ResponseInterface;
+use Hyperf\Utils\Context;
+use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
 
 class Response
 {
@@ -47,5 +50,28 @@ class Response
             'code' => $code,
             'data' => $data,
         ]);
+    }
+
+
+    public function redirect($url, $status = 302)
+    {
+        return $this->response()
+            ->withAddedHeader('Location', (string) $url)
+            ->withStatus($status);
+    }
+
+    public function cookie(Cookie $cookie)
+    {
+        $response = $this->response()->withCookie($cookie);
+        Context::set(PsrResponseInterface::class, $response);
+        return $this;
+    }
+
+    /**
+     * @return \Hyperf\HttpMessage\Server\Response
+     */
+    public function response()
+    {
+        return Context::get(PsrResponseInterface::class);
     }
 }
---
+++ app/Middleware/CoreMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Middleware;
+
+
+use App\Kernel\Http\Response;
+use Hyperf\Utils\Context;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CoreMiddleware implements MiddlewareInterface
+{
+    /** @var ContainerInterface */
+    protected $container;
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // TODO: Implement process() method.
+        $response = di()->get(Response::class)->response()->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Allow-Headers', '*');
+        /*        $response = di()->get(Response::class)->response()->withHeader('Access-Control-Allow-Origin', '*')
+                    ->withHeader('Access-Control-Allow-Credentials', 'true')
+                    ->withHeader('Access-Control-Allow-Headers', 'Origin, DNT, Keep-Alive, User-Agent, Cache-Control, Content-Type, Authorization');*/
+
+        Context::set(ResponseInterface::class, $response);
+
+        if ($request->getMethod() == 'OPTIONS') {
+            return $response;
+        }
+
+        return $handler->handle($request);
+    }
+}
