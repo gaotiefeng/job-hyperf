0eb3616e7741522e1b992a4b4238236ce423d02d
gaotiefeng <1096392101@qq.com>
2020-04-17 10:30:01 +0800

    add setup.mysql

--- .build/setup.mysql.sh
+++ .build/setup.mysql.sh
@@ -4,8 +4,8 @@ CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$(dirname $(dirname $CURRENT_DIR))}"
 
 echo -e "Create MySQL database..."
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS note_xcx charset=utf8mb4 collate=utf8mb4_unicode_ci;"
-cat "${TRAVIS_BUILD_DIR}/.build/job.sql" | mysql -u root note_xcx
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS job charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+cat "${TRAVIS_BUILD_DIR}/.build/job.sql" | mysql -u root job
 
 echo -e "Done\n"
 
